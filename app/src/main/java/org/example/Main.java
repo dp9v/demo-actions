9da52c62ac0a579cae27e5171ef0117eaf68f4b0
dmitrii <polkovnikov13@gmail.com>
2024-02-13 17:42:55 +0100

    Add test

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
